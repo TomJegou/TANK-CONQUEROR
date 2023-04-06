06dbfac862eabad0e9c563043f214fc2771871bf
L0uxe <loulourxl@hotmail.com>
2023-04-06 09:13:45 +0200

    fixing bug

--- ia.js
+++ ia.js
@@ -1,21 +1,17 @@
-//return a random case
+/**return a random case*/
 const randomcase =()=>{return ["abcdefghij"[Math.floor(Math.random() * 10)],Math.floor(Math.random()*10+1)]}
 
-//return true if the case is already play( is in the tab)
+/**return true if the case is already play (is in the tab)*/
 const checkCaseAlreadyPlay=(tab,caseToCheck)=>{
-    console.log(tab)
-    console.log(tab.length)
+
     for (let elemenInTab = 0; elemenInTab < tab.length; elemenInTab++) {
-            if(tab[elemenInTab][0][1] === caseToCheck[0][1]){
+            if(tab[elemenInTab][0] === caseToCheck[0] && tab[elemenInTab][1] === caseToCheck[1]){
                 return true
             }
         }
         return false
     }
 
-test1=[[1,'b'],[3,'o'],[5,'o'],[4,'a']]
-test2=[4,'a']
-console.log(checkCaseAlreadyPlay(test1,test2))
 
 /** 
 the first ia shoot on a random case 
@@ -25,29 +21,30 @@ the first ia shoot on a random case
 */
 function ia1(response,tab) {
     let attackCase =randomcase()
-    let caseAlreadyPlay=[]
     console.log(attackCase)
-
-    if (checkCaseAlreadyPlay(tab,attackCase)) {
+    let caseAlreadyPlay=[['a',1]]
+    console.log("case played : ")
+    console.log(caseAlreadyPlay)
+    if (checkCaseAlreadyPlay(caseAlreadyPlay,attackCase)) {
         attackCase = randomcase()
-        console.log(attackCase)
-        
+        console.log("new case :",attackCase)
+        caseAlreadyPlay.push(attackCase)
+        console.log("New tab of case played : ")
+        console.log(caseAlreadyPlay)
+    }else{
+        caseAlreadyPlay.push(attackCase)
+        console.log("New tab of case played : ")
+        console.log(caseAlreadyPlay)
     }
-    //need to check is case as already play
-    //check the response to know if the shoot hit or no
-    //if it hit shoot near by
-    //if it miss shoot on a random case
 
-    console.log(attackCase)
-    console.log(attackCase)
 
     if(response===0 || 1){
 
     }
-    caseAlreadyPlay.push(attackCase)
-    console.log(caseAlreadyPlay)
+   
     return (attackCase)
 }
+
 function ia2(){
 
 }
@@ -55,3 +52,4 @@ function ia2(){
 function ia3(){
 
 }
+ia1()
