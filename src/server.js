b30a5649e261e061c8fb001d35a2d64cf6572b0f
TomJegou <tomjegou10@gmail.com>
2023-04-07 10:31:02 +0200

    semi colon

--- src/server.js
+++ src/server.js
@@ -7,22 +7,22 @@ const port = 8000;
 const urlFilserverHtml = `http://app:8000/html`;
 
 export default function StartServer (){
-    app.use(express.static('./public'))
+    app.use(express.static('./public'));
 
     app.get('/', (req, res) => {
         req.pipe(request(`${urlFilserverHtml}/index.html`)).pipe(res);
     });
 
     app.get('/tuto', (req, res) => {
-        res.send(`Welcome to tuto`)
+        res.send(`Welcome to tuto`);
     });
 
     app.get('/solo', (req, res) => {
-        req.pipe(request(`${urlFilserverHtml}/menuIA.html`)).pipe(res)
+        req.pipe(request(`${urlFilserverHtml}/menuIA.html`)).pipe(res);
     });
 
     app.get('/multi', (req, res) => {
-        res.send(`Welcome to multi`)
+        res.send(`Welcome to multi`);
     });
 
     app.listen(port, ()=>{
