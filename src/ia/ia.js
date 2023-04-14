44426c8dda769b1eb780f1500e2840f87e511ac0
L0uxe <loulourxl@hotmail.com>
2023-04-14 11:54:34 +0200

    wip

--- src/ia/ia.js
+++ src/ia/ia.js
@@ -175,23 +175,31 @@ function diagonalLine() {
     }
     return caseDiagonalLine;
   }
-console.log(diagonalLine())
+
 
 function diagonalAttack(caseAlreadyPlay,attackCase){
     let findCase = false    
     while(!findCase){
-        if (checkCaseAlreadyPlay(caseAlreadyPlay,attackCase)) {
-            attackCase = di()
-            caseAlreadyPlay.push(attackCase)
-            findCase=true
-        }else{
-            caseAlreadyPlay.push(attackCase)
-            findCase=true
+        for(let i =0;i<diagonalLine.length;i++){
+            if (checkCaseAlreadyPlay(caseAlreadyPlay,diagonalAttack[i])) {
+                attackCase = diagonalAttack[i]
+                caseAlreadyPlay.push(attackCase)
+                findCase=true
+                break
+            }else{
+                for(let i =0;i<allDiagonalCase.length;i++){
+                    if(checkCaseAlreadyPlay(caseAlreadyPlay,allDiagonalCase[i])) {{
+                        attackCase = allDiagonalCase[i]
+                        caseAlreadyPlay.push(attackCase)
+                        findCase=true
+                        break
+                    }}
+                }
+            }
         }
-        break
-    
+    }
 }
-}
+
 /** 
 the second ia shoot case in diagonal  
 ** check the response to know if the shoot hit or no
@@ -199,10 +207,80 @@ the second ia shoot case in diagonal
 ** if it miss shoot on a random case
 */
 function ia2(response,caseAlreadyPlay){
-    
+    //need to change
+    let attackCase = [1,1]
+    for(let i =0;i<diagonalLine.length;i++){
+        if (checkCaseAlreadyPlay(caseAlreadyPlay,diagonalLine[i])) {
+            attackCase =diagonalLine()[i]
+        }else{
+            attackCase = allDiagonalCase[Math.floor(Math.random()*50+1)]//random case in diagonal case
+        }
+    }
+    if(response == "toucher"){
+        hitCase.push(caseAlreadyPlay[caseAlreadyPlay.length-1])
+        if(targeting == true){
+            if(hitCase>2){
+            for(let k = 0;i<findLineDirection(hitCase).length;k++){
+                    if(!checkCaseAlreadyPlay(caseAlreadyPlay,findLineDirection(hitCase)[k])){
+                        caseAlreadyPlay.push(attackCase)
+                        attackCase = findLineDirection(hitCase)[k]
+                        nb+=1
+                        break
+                    }
+                }
+            }else{
+            for(let j = 0;j<findCaseAround(caseAlreadyPlay[caseAlreadyPlay.length-nb]).length;j++){
+                if(!checkCaseAlreadyPlay(caseAlreadyPlay,findCaseAround(caseAlreadyPlay[caseAlreadyPlay.length-nb])[j])) {
+                    attackCase = findCaseAround(caseAlreadyPlay[caseAlreadyPlay.length-nb])[j]
+                    caseAlreadyPlay.push(attackCase)
+                    nb+=1
+                    break
+                }
+            }
+        }
+        }else if(targeting==false){
+        for(let i = 0 ;i<findCaseAround(caseAlreadyPlay[caseAlreadyPlay.length-1]).length;i++){
+            if (!checkCaseAlreadyPlay(caseAlreadyPlay,findCaseAround(caseAlreadyPlay[caseAlreadyPlay.length-1])[i])) {
+                attackCase = findCaseAround(caseAlreadyPlay[caseAlreadyPlay.length-1])[i]
+                caseAlreadyPlay.push(attackCase)
+                targeting = true
+                break
+            }
+        }
+    }
+    else{
+        diagonalAttack(caseAlreadyPlay,attackCase)
+    }
+    }
+    if(response == "louper"){
+        if(targeting == true){
+            for(let j = 0;j<findCaseAround(caseAlreadyPlay[caseAlreadyPlay.length-nb]).length;j++){
+                if(!checkCaseAlreadyPlay(caseAlreadyPlay,findCaseAround(caseAlreadyPlay[caseAlreadyPlay.length-nb])[j])) {
+                    attackCase = findCaseAround(caseAlreadyPlay[caseAlreadyPlay.length-nb])[j]
+                    caseAlreadyPlay.push(attackCase)
+                    break
+                }
+            }
+        }
+        else if (targeting==false){
+            diagonalAttack(caseAlreadyPlay,attackCase)
+        }
+    }
+    if(response == "detruit"){
+        hitCase=[]
+        nb=1
+        targeting=false
+        diagonalAttack(caseAlreadyPlay,attackCase)
+    }
+    return (caseAlreadyPlay)
 }
 /** 
 the third ia use the Nick berry algorithm
 */
 function ia3(){
 }
+
+
+let tab = [[1,1]]
+ia2("louper",tab)
+console.log(tab)
