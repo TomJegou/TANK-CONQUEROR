8dc41f30943354e933212beae53e94899d32db61
TomJegou <tomjegou10@gmail.com>
2023-04-27 15:13:11 +0200

    create component

--- src/components/FieldBox.js
+++ src/components/FieldBox.js
@@ -1,3 +1,5 @@
-export default function FieldBox() {
-    return 
+export default function FieldBox({ text }) {
+    return (
+        <div className="flex flex-row flex-wrap justify-center items-center w-[5vh] h-[5vh]">{text}</div>
+    )
 }
--- src/components/TankBox.js
+++ src/components/TankBox.js
@@ -2,9 +2,9 @@ import { motion } from "framer-motion"
 
 export default function TankBox({ text, color }) {
     return (
-        <motion.div className=""
+        <motion.div className="flex flex-row flex-wrap justify-center items-center w-[5vh] h-[5vh]"
         animate = {{ backgroundColor: ["", color], opacity: [0, 100]}}
-        transition={{duration: 1}}>
+        transition={{duration: 0.5}}>
             {text}
         </motion.div>
     )
