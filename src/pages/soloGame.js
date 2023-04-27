466bd2a889e0d7ce0c27968ef3dc0a6802ba4802
TomJegou <tomjegou10@gmail.com>
2023-04-27 15:39:27 +0200

    Front title

--- src/pages/soloGame.js
+++ src/pages/soloGame.js
@@ -5,7 +5,7 @@ import GameSet from "@/components/gameSet"
 export default function SoloGame() {
     return (
         <Layout className="flex flex-row flex-wrap justify-center">
-            <h1 className="flex flex-row flex-wrap justify-center w-full">SOLO GAME</h1>
+            <h1 className="flex flex-row flex-wrap justify-center w-full text-4xl">SOLO GAME</h1>
             <GameSet />
         </Layout>
     )
