f5bda789b74027ca02ca79e0194ef8193e3ad226
TomJegou <tomjegou10@gmail.com>
2023-04-27 15:13:16 +0200

    refactoring

--- src/components/Box.js
+++ src/components/Box.js
@@ -1,39 +1,22 @@
 import { AllTanksEnemy, AllTanksPlayer } from "@/utils/generateTanks"
 import { IsOccupied } from "@/utils/tools"
-import { motion } from "framer-motion"
+import FieldBox from "./FieldBox"
+import TankBox from "./TankBox"
 
-export default function Box ({ id, x, y, side }) {
-    let text = ""
+export default function Box ({ id, x, y, side, text }) {
     const debugMode = true
-    let color = ""
-    let grid = []
-    side == "enemy" ? grid = AllTanksEnemy : grid = AllTanksPlayer
+    const grid = side == "enemy" ? AllTanksEnemy : AllTanksPlayer
     const isOc = IsOccupied({x: x, y: y}, grid)
-    function HandleClick() {
-        console.log(isOc)
-    }
-
-    if (debugMode){
-        if(side == "player") {
-            isOc ? color = "blue" : color = ""
-        } else{
-            isOc ? color = "red" : color = ""
-        }
-    } else {
-        if(side == "player") {
-            isOc ? color = "blue" : color = ""
-        }
-    }
+    const color = debugMode ? side == "player" ? isOc ? "blue": "" : isOc? "red" : "" : side == "player" ? isOc ? "blue" : "" :  ""
+    const ContentBox = isOc ? TankBox : FieldBox
 
     return (
-        <motion.div className="flex h-[5vh] w-[5vh] border"
-        animate = {{ backgroundColor: ["", color], opacity: [0, 100]}}
-        transition={{duration: 1}}
-        id={id} 
-        x={x} 
-        y={y} 
-        side={side}
-        onClick={HandleClick}
-        >{text}</motion.div>
+        <div className="flex h-[5vh] w-[5vh] border"
+        id={id}
+        x={x}
+        y={y}
+        side={side}>
+            <ContentBox color={color} text={text}/>
+        </div>
     )
 }
