96fba8a2879f590bb7e2c58b9c05af8c1f8bc05d
TomJegou <tomjegou10@gmail.com>
2023-04-28 19:51:56 +0200

    create state variales

--- src/pages/soloGame.js
+++ src/pages/soloGame.js
@@ -4,19 +4,23 @@ import { GenerateTank, AllTanksEnemy, AllTanksPlayer } from "@/utils/generateTan
 import { engine, getNumBoxToBeTouched } from "@/utils/engine"
 import EndGame from "@/components/endGame"
 import { useState } from "react"
+import { getRandomInt } from "@/utils/tools"
 
 export default function SoloGame() {
     GenerateTank()
     const [isGameOver, setIsGameOver] = useState(false)
     const [acclamation, setAcclamation] = useState("")
     const [winnerName, setWinnerName] = useState("")
-    const [numBoxTobeTouchedByPlayer, setNumBoxTobeTouchedByPlayer] = useState(getNumBoxToBeTouched(AllTanksEnemy))
-    const [numBoxTobeTouchedByEnemy, setNumBoxTobeTouchedByEnemy] = useState(getNumBoxToBeTouched(AllTanksPlayer))
+    const [numBoxTobeTouchedByPlayer, setNumBoxTobeTouchedByPlayer] = useState(getNumBoxToBeTouched(AllTanksEnemy)-1)
+    const [numBoxTobeTouchedByEnemy, setNumBoxTobeTouchedByEnemy] = useState(getNumBoxToBeTouched(AllTanksPlayer)-1)
+    const [boxPlayedByIA, setBoxPlayedByIA] = useState([])
+    const [boxPlayedByPlayer, setBoxPlayedByPlayer] = useState([])
+    const [whosTurn, setWhosTurn] = useState(Math.random() > .5 ? "player" : "IA")
     
-    const handleDataFromGameSet = (boxClicked) => {
+    const handleDataFromEnemyGrid = (boxClicked) => {
         let responseEngine = engine(AllTanksEnemy, boxClicked)
         if (responseEngine != "missed") {
-            numBoxTobeTouchedByPlayer--
+            setNumBoxTobeTouchedByPlayer(a => a - 1)
             if (numBoxTobeTouchedByPlayer <= 0) {
                 setAcclamation("Congratulation !")
                 setWinnerName("player")
@@ -28,7 +32,7 @@ export default function SoloGame() {
     return (
         <Layout className="flex flex-row flex-wrap justify-center bg-black">
             <h1 className="flex flex-row flex-wrap justify-center w-full text-4xl text-white">SOLO GAME</h1>
-            <GameSet sendResponseToSoloGame={handleDataFromGameSet} />
+            <GameSet sendResponseToSoloGame={handleDataFromEnemyGrid} />
             <EndGame acclamation={acclamation} winner={winnerName} isGameOver={isGameOver} />
         </Layout>
     )
