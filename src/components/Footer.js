388d8522d58ff202fab67ab80cacd18010bf727b
TomJegou <tomjegou10@gmail.com>
2023-05-01 19:39:42 +0200

    add possibility to set classname from parent component

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,8 @@
 import Link from "next/link"
 
-export default function Footer() {
+export default function Footer({ className }) {
     return (
-        <div className="flex flex-col flex-wrap justify-center items-center w-full h-[15vh] bg-white">
+        <div className={"flex flex-col flex-wrap justify-center items-center w-full h-[15vh] bg-white " + className}>
             <h1 className="flex flex-row flex-wrap justify-center items-center">Made by :</h1>
             <Link href={"https://github.com/TomJegou"}>Tomyj</Link>
             <Link href={"https://github.com/L0uxe"}>L0uxe</Link>
