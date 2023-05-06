01fe8d39cd6b8d36e2b5fecbaf0c191e0209ca13
TomJegou <tomjegou10@gmail.com>
2023-05-06 17:37:13 +0200

    adding whileHoover

--- src/components/FieldBox.js
+++ src/components/FieldBox.js
@@ -1,9 +1,11 @@
+import { motion } from "framer-motion"
+
 export default function FieldBox({ text }) {
     return (
-        <div className="flex flex-row flex-wrap justify-center items-center w-[5vh] h-[5vh]">
+        <motion.div className="flex flex-row flex-wrap justify-center items-center w-[5vh] h-[5vh]" whileHover={{backgroundColor: "white"}}>
             <h1 className="flex flex-row flex-wrap justify-center items-center font-bold text-4xl text-white">
                 {text}
             </h1>
-        </div>
+        </motion.div>
     )
 }
--- src/components/TankBox.js
+++ src/components/TankBox.js
@@ -3,8 +3,9 @@ import { motion } from "framer-motion"
 export default function TankBox({ text, color }) {
     return (
         <motion.div className="flex flex-row flex-wrap justify-center items-center w-[5vh] h-[5vh]"
-        animate = {{ backgroundColor: ["", color], opacity: [0, 100]}}
-        transition={{duration: 0.5}}>
+        animate = {{backgroundColor: ["", color], opacity: [0, 100]}}
+        transition={{duration: 0.5}}
+        whileHover={{backgroundColor: "white"}}>
             <h1 className="flex flex-row flex-wrap justify-center items-center font-bold text-4xl text-white">
                 {text}
             </h1>
