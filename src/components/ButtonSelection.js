14a80e881854d38cfddecebfaaa08095df9f38d1
TomJegou <tomjegou10@gmail.com>
2023-05-06 12:33:46 +0200

    starting animation

--- src/components/ButtonSelection.js
+++ src/components/ButtonSelection.js
@@ -1,7 +1,11 @@
 import Link from "next/link";
 import { setMapWorld, setIADifficulty, setMode } from "@/utils/gameSetup";
+import { useState, useEffect } from "react";
+import { motion } from "framer-motion";
 
 export default function ButtonSelection({ href, text, keyWord }) {
+    const [isHoover, setIsHoover] = useState(false)
+
     const handleClick = () => {
         switch (keyWord){
             case "mapWorld":
@@ -15,9 +19,17 @@ export default function ButtonSelection({ href, text, keyWord }) {
                 break
         }
     }
+
+    const handleHoover = () => {
+        setIsHoover(true)
+    }
+
     return (
-        <div onClick={handleClick}>
+        <motion.div onClick={handleClick} className="flex flex-col flex-wrap" whileHover={handleHoover}>
             <Link className="flex flex-row flex-wrap justify-center items-center h-11" href={href}>{text}</Link>
-        </div>
+            <motion.div 
+            className="bg-black h-1 rounded-sm"
+            animate={{scaleX: isHoover ? [2, 1] : null}}/>
+        </motion.div>
     )
 }
