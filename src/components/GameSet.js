c2d75bc617d2d4e2698760f541e4303cdc405624
TomJegou <tomjegou10@gmail.com>
2023-05-06 11:54:21 +0200

    add div to display whosturn

--- src/components/GameSet.js
+++ src/components/GameSet.js
@@ -9,6 +9,11 @@ export default function GameSet({ sendResponseToSoloGame, debugMode, whosTurn, b
                 <Grid side="player" sendResponseToGameSet={sendResponseToSoloGame} debugMode={debugMode} whosTurn={whosTurn} boxPlayedByEnely={boxPlayedByEnemy} />
             </div>
 
+            <div className="flex flex-row flex-wrap items-center gap-3">
+                <p className="text-white text-2xl flex flex-row flex-wrap">turn: </p>
+                <p className="text-white text-2xl flex flex-row flex-wrap">{whosTurn}</p>
+            </div>
+
             <div className="flex flex-col justify-around h-[80vh]">
                 <h1 className="flex flex-row flex-wrap justify-center items-center text-white text-4xl">The opponent</h1>
                 <Grid side="enemy" sendResponseToGameSet={sendResponseToSoloGame} debugMode={debugMode} whosTurn={whosTurn} boxPlayedByEnely={boxPlayedByEnemy} />
