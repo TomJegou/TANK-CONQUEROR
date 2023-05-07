ed8d432159166542254b15003958faf6100a4d4c
TomJegou <tomjegou10@gmail.com>
2023-05-07 12:26:45 +0200

    adding background

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,9 +1,12 @@
 import ButtonSelection from "./ButtonSelection"
+import { motion } from "framer-motion"
 
 export default function Menu({ title, listChoices, keyWord }) {
     return (
         <>
-            <h1 className="absolute flex flex-row flex-wrap justify-center text-4xl top-11">{title}</h1>
+            <motion.div className="absolute flex-row flex-wrap justify-center items-center top-11 border-black rounded-xl border-solid h-[10vh] w-[30vw] border-[0.5rem]" animate={{scale: [0, 1]}} transition={{duration: 1}}>
+                <motion.h1 className="absolute top-[2.5vh] flex flex-row flex-wrap justify-center text-4xl w-full h-[5vh]">{title}</motion.h1>
+            </motion.div>
             <div className="absolute flex flex-col flex-wrap justify-center items-center h-[15vh] top-52">
                 {listChoices.map((link, index) => {
                     return <ButtonSelection key={index} href={link.href} text={link.text} keyWord={keyWord}/>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,13 @@
 import Layout from "@/components/Layout"
 import Menu from "@/components/Menu"
+import Image from "next/image"
+import Background from "../../public/background.JPG"
 
 export default function Home() {
   return (
     <Layout>
       <div className="flex justify-center h-[85vh] w-[100vw]">
+          <Image src={Background} alt="background" className="w-full h-full z-[-1]"/>
           <Menu title={"Welcome to Tank Conqueror"} 
           listChoices={[
             {text: "Solo", href: "/mapMenu"},
