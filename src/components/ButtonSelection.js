9956ae59bd589e938c5eb67a8f75077f96ceb22a
TomJegou <tomjegou10@gmail.com>
2023-05-07 11:17:54 +0200

    change height undeline

--- src/components/ButtonSelection.js
+++ src/components/ButtonSelection.js
@@ -36,7 +36,7 @@ export default function ButtonSelection({ href, text, keyWord }) {
     return (
         <motion.div onClick={handleClick} className="flex flex-col flex-wrap" whileHover={handleHoover} onHoverStart={handleHooverStart} onHoverEnd={handleHooverEnd}>
             <Link className="flex flex-row flex-wrap justify-center items-center h-11" href={href}>{text}</Link>
-            <div ref={scope} className="bg-black h-[0.1vh] rounded-sm" style={{opacity: isLoad ? 1 : 0}} />
+            <motion.div ref={scope} className="bg-black h-[0.2vh] rounded-sm" animate={{scale: isLoad ? 1 : 0}} transition={{duration: .5, type: "spring", damping: 10}} />
         </motion.div>
     )
 }
