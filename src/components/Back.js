fcb1db66041c15db268aef679f121e71a1b8e7b2
TomJegou <tomjegou10@gmail.com>
2023-05-07 23:42:43 +0200

    adding style

--- src/components/Back.js
+++ src/components/Back.js
@@ -5,7 +5,7 @@ import Link from "next/link"
 export default function Back({ href }) {
     return (
         <Link href={href} className="absolute flex left-5 top-5">
-            <IconContext.Provider value={{ color: "black", size: "2em"}}>
+            <IconContext.Provider value={{ color: "white", size: "2em"}}>
                 <IoArrowBackCircleSharp />
             </IconContext.Provider>
         </Link>
