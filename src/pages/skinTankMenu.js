4b5f893f8c434179d068c814823889e1de413317
TomJegou <tomjegou10@gmail.com>
2023-05-08 23:49:22 +0200

    wip

--- src/pages/skinTankMenu.js
+++ src/pages/skinTankMenu.js
@@ -1,3 +1,14 @@
+import Layout from "@/components/Layout"
+import skinTankGreen from "@/../public/skinTanks/tankGreen.png"
+import skinTankBlue from "@/../public/skinTanks/tankBlue.png"
+import skinTankBrown from "@/../public/skinTanks/tankBrown.png"
+
 export default function SkinTankMenu () {
-    
+    return (
+        <Layout>
+            <div className="flex justify-center h-[85vh] w-[100vw]">
+
+            </div>
+        </Layout>
+    )
 }
