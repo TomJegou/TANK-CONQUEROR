fd1991726893c6873603e92be0162e8591c28969
TomJegou <tomjegou10@gmail.com>
2023-05-08 18:04:37 +0200

    improve box

--- src/components/PopUpTuto.js
+++ src/components/PopUpTuto.js
@@ -37,7 +37,7 @@ export default function PopUpTuto({ children, nbrClick,trigerNumberClick, funcSe
     }
 
     return (
-        <div ref={scope} className="absolute top-10 h-[88vh] w-[88vw] flex flex-row flex-wrap justify-center items-center rounded-3xl text-white backdrop-blur-xl shadow-2xl shadow-black bg-green-military/50">
+        <div ref={scope} className="absolute top-10 h-[88vh] w-[88vw] flex flex-row flex-wrap justify-center items-center rounded-3xl text-white backdrop-blur-xl shadow-[12px_12px_2px_1px] shadow-black/50 bg-green-military/50">
             {children}
             <div className="absolute h-[5vh] w-[5vw] border-2 rounded-3xl bottom-4 right-10  flex flex-row flex-wrap justify-center items-center"
             {...attrtibuteNext}
