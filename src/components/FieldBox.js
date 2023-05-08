3fd5ea60e72ad4372d486f9cd3af6f1d14288539
TomJegou <tomjegou10@gmail.com>
2023-05-08 11:35:49 +0200

    add style for cursor pointer

--- src/components/FieldBox.js
+++ src/components/FieldBox.js
@@ -2,10 +2,13 @@ import { motion } from "framer-motion"
 
 export default function FieldBox({ text }) {
     return (
-        <motion.div className="flex flex-row flex-wrap justify-center items-center w-[5vh] h-[5vh]" whileHover={{backgroundColor: "white"}}>
-            <h1 className="flex flex-row flex-wrap justify-center items-center font-bold text-4xl text-white">
-                {text}
-            </h1>
+        <motion.div
+            className="flex flex-row flex-wrap justify-center items-center w-[5vh] h-[5vh]"
+            whileHover={{backgroundColor: "white"}}
+            style={{cursor: "pointer"}}>
+                <h1 className="flex flex-row flex-wrap justify-center items-center font-bold text-4xl text-white">
+                    {text}
+                </h1>
         </motion.div>
     )
 }
--- src/components/TankBox.js
+++ src/components/TankBox.js
@@ -5,7 +5,8 @@ export default function TankBox({ text, color }) {
         <motion.div className="flex flex-row flex-wrap justify-center items-center w-[5vh] h-[5vh]"
         animate = {{backgroundColor: ["", color], opacity: [0, 100]}}
         transition={{duration: 0.75}}
-        whileHover={{backgroundColor: "white"}}>
+        whileHover={{backgroundColor: "white"}}
+        style={{cursor: "pointer"}}>
             <h1 className="flex flex-row flex-wrap justify-center items-center font-bold text-4xl text-white">
                 {text}
             </h1>
