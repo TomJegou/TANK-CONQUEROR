5030f60668e8607aed68e0d78a03dd6e9d4cf653
TomJegou <tomjegou10@gmail.com>
2023-05-08 18:30:43 +0200

    change title w and h

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -7,7 +7,7 @@ export default function Menu({ title, listChoices, keyWord, sendDataToPage, text
             <motion.h1
                 animate={{scale: [0, 1]}}
                 transition={{duration: 1}}
-                className="absolute top-[6vh] flex flex-row flex-wrap justify-center text-7xl w-full border-solid border-[0.5rem] rounded-xl"
+                className="absolute top-[6vh] flex flex-row flex-wrap justify-center items-center text-7xl w-[80vw] h-[15vh] border-solid border-[0.5rem] rounded-xl"
                 style={{color: textColor, borderColor: textColor}}>
                     {title}
             </motion.h1>
