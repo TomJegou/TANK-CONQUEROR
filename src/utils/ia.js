95f43c5663f4572c5dbd052b23ae1c0b1c6f8652
TomJegou <tomjegou10@gmail.com>
2023-05-10 01:37:34 +0200

    refacotring

--- src/utils/ia.js
+++ src/utils/ia.js
@@ -14,6 +14,73 @@ export default class IA {
         return this.listBoxAlreadyPlayed.includes(box)
     }
 
+    attack (respFromEngine) {
+        if (this.currentMode == "search") {
+            if (respFromEngine == "touched") {
+                this.firstBoxTouchedWhenFound = this.getLastShot()
+                this.currentMode = "finish"
+                return this.finish (respFromEngine)
+            } else if (respFromEngine == "missed") {
+                return this.search()
+            } else if (respFromEngine == "sinked") {
+                this.StateSensFoundedTank == "unknown" ? "" : this.StateSensFoundedTank == "unknown"
+                return this.search()
+            } else {
+                return this.search()
+            }
+        } else if (this.currentMode == "finish") {
+            return this.finish(respFromEngine)
+        }
+    }
+
+    finish (respFromEngine) {
+        const boxAround = [
+            {x: this.firstBoxTouchedWhenFound.x + 1 <= 10 ? this.firstBoxTouchedWhenFound.x + 1 : this.firstBoxTouchedWhenFound.x, y: this.firstBoxTouchedWhenFound.y},
+            {x: this.firstBoxTouchedWhenFound.x - 1 >= 1 ? this.firstBoxTouchedWhenFound.x - 1 : this.firstBoxTouchedWhenFound.x, y: this.firstBoxTouchedWhenFound.y},
+            {x: this.firstBoxTouchedWhenFound.x, y: this.firstBoxTouchedWhenFound.y + 1 <= 10 ? this.firstBoxTouchedWhenFound.y + 1: this.firstBoxTouchedWhenFound.y},
+            {x: this.firstBoxTouchedWhenFound.x, y: this.firstBoxTouchedWhenFound.y - 1 >= 1 ? this.firstBoxTouchedWhenFound.y - 1: this.firstBoxTouchedWhenFound.y},
+        ]
+        if (this.StateSensFoundedTank == "unknown") {
+            return this.fireAround(boxAround)
+        } else if (this.StateSensFoundedTank == "searching") {
+            if (respFromEngine == "touched") {
+                this.StateSensFoundedTank = "founded"
+                this.getDirection()
+                return this.fireKnowingDir()
+            } else if (respFromEngine == "sinked") {
+                this.StateSensFoundedTank = "unknown"
+                this.currentMode = "search"
+                return this.search()
+            } else {
+                return this.fireAround(boxAround)
+            }
+        } else {
+            if (respFromEngine == "sinked") {
+                this.StateSensFoundedTank = "unknown"
+                this.direction = ""
+                this.currentMode = "search"
+                return this.search()
+            } else if (respFromEngine == "missed") {
+                if (this.sensFoundedTank == "horizontale") {
+                    if (this.direction == "right"){
+                        this.direction = "left"
+                    } else {
+                        this.direction = "right"
+                    }
+                } else {
+                    if (this.direction == "up"){
+                        this.direction = "down"
+                    } else {
+                        this.direction = "up"
+                    }
+                }
+                return this.fireKnowingDir()
+            } else {
+                return this.fireKnowingDir()
+            }
+        }
+    }
+
     fireAround (boxAround) {
         let result = {x: 1, y: 1}
         boxAround.map(box => {
--- src/utils/iaSimple.js
+++ src/utils/iaSimple.js
@@ -6,25 +6,6 @@ export default class IASimple extends IA {
         this.generateCheckerboard()
     }
 
-    attack (respFromEngine) {
-        if (this.currentMode == "search") {
-            if (respFromEngine == "touched") {
-                this.firstBoxTouchedWhenFound = this.getLastShot()
-                this.currentMode = "finish"
-                return this.finish (respFromEngine)
-            } else if (respFromEngine == "missed") {
-                return this.search()
-            } else if (respFromEngine == "sinked") {
-                this.StateSensFoundedTank == "unknown" ? "" : this.StateSensFoundedTank == "unknown"
-                return this.search()
-            } else {
-                return this.search()
-            }
-        } else if (this.currentMode == "finish") {
-            return this.finish(respFromEngine)
-        }
-    }
-
     search () {
         let result = {x: 1, y: 1}
         this.listBoxToPlayForSearch.map(box => {
@@ -35,54 +16,6 @@ export default class IASimple extends IA {
         })
         this.listBoxAlreadyPlayed.push(result)
         return result
-    }
-
-    finish (respFromEngine) {
-        const boxAround = [
-            {x: this.firstBoxTouchedWhenFound.x + 1 <= 10 ? this.firstBoxTouchedWhenFound.x + 1 : this.firstBoxTouchedWhenFound.x, y: this.firstBoxTouchedWhenFound.y},
-            {x: this.firstBoxTouchedWhenFound.x - 1 >= 1 ? this.firstBoxTouchedWhenFound.x - 1 : this.firstBoxTouchedWhenFound.x, y: this.firstBoxTouchedWhenFound.y},
-            {x: this.firstBoxTouchedWhenFound.x, y: this.firstBoxTouchedWhenFound.y + 1 <= 10 ? this.firstBoxTouchedWhenFound.y + 1: this.firstBoxTouchedWhenFound.y},
-            {x: this.firstBoxTouchedWhenFound.x, y: this.firstBoxTouchedWhenFound.y - 1 >= 1 ? this.firstBoxTouchedWhenFound.y - 1: this.firstBoxTouchedWhenFound.y},
-        ]
-        if (this.StateSensFoundedTank == "unknown") {
-            return this.fireAround(boxAround)
-        } else if (this.StateSensFoundedTank == "searching") {
-            if (respFromEngine == "touched") {
-                this.StateSensFoundedTank = "founded"
-                this.getDirection()
-                return this.fireKnowingDir()
-            } else if (respFromEngine == "sinked") {
-                this.StateSensFoundedTank = "unknown"
-                this.currentMode = "search"
-                return this.search()
-            } else {
-                return this.fireAround(boxAround)
-            }
-        } else {
-            if (respFromEngine == "sinked") {
-                this.StateSensFoundedTank = "unknown"
-                this.direction = ""
-                this.currentMode = "search"
-                return this.search()
-            } else if (respFromEngine == "missed") {
-                if (this.sensFoundedTank == "horizontale") {
-                    if (this.direction == "right"){
-                        this.direction = "left"
-                    } else {
-                        this.direction = "right"
-                    }
-                } else {
-                    if (this.direction == "up"){
-                        this.direction = "down"
-                    } else {
-                        this.direction = "up"
-                    }
-                }
-                return this.fireKnowingDir()
-            } else {
-                return this.fireKnowingDir()
-            }
-        }
     }
 
     generateCheckerboard () {
