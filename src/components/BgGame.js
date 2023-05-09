4294781ac34de4a2ce2735eeaf49feb0d639ac85
TomJegou <tomjegou10@gmail.com>
2023-05-09 21:14:46 +0200

    ref

--- src/components/BgGame.js
+++ src/components/BgGame.js
@@ -1,7 +1,7 @@
 import Image from "next/image"
 
-export default function BgGame ({ src, alt }) {
+export default function BgGame ({ src }) {
     return (
-        <Image src={src} alt={alt} className="absolute flex top-0 left-0 bottom-0 w-full h-full z-[-1]" />
+        <Image src={src} alt="Bg" className="absolute flex top-0 left-0 bottom-0 w-full h-full z-[-1]" />
     )
 }
