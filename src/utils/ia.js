6a3b5d01ec487ae91c9d841c35131226893f4824
TomJegou <tomjegou10@gmail.com>
2023-05-10 01:33:57 +0200

    finished method to finish tank

--- src/utils/ia.js
+++ src/utils/ia.js
@@ -9,9 +9,11 @@ export default class IA {
     getLastShot () {
         return this.listBoxAlreadyPlayed[this.listBoxAlreadyPlayed.length - 1]
     }
+    
     isAlreadyPlayed (box) {
         return this.listBoxAlreadyPlayed.includes(box)
     }
+
     fireAround (boxAround) {
         let result = {x: 1, y: 1}
         boxAround.map(box => {
@@ -23,5 +25,37 @@ export default class IA {
         this.listBoxAlreadyPlayed.push(result)
         this.StateSensFoundedTank = "searching"
         return result
+    }
+
+    getDirection () {
+        const lastBoxPlayed = this.getLastShot()
+        if (this.firstBoxTouchedWhenFound.x + 1 == lastBoxPlayed.x || this.firstBoxTouchedWhenFound.x -1 == lastBoxPlayed.x) {
+            this.sensFoundedTank == "horizontale"
+            if (this.firstBoxTouchedWhenFound.x + 1 == lastBoxPlayed.x) {
+                this.direction = "right"
+            } else {
+                this.direction = "left"
+            }
+        } else {
+            this.sensFoundedTank == "verticale"
+            if (this.firstBoxTouchedWhenFound.y + 1 == lastBoxPlayed.y) {
+                this.direction = "down"
+            } else {
+                this.direction = "up"
+            }
+        }
+    }
+
+    fireKnowingDir() {
+        const lasBox = this.getLastShot()
+        if (this.direction == "right") {
+            return {x: lasBox.x + 1, y: lasBox.y}
+        } else if (this.direction == "left") {
+            return {x: lasBox.x - 1, y: lasBox.y}
+        } else if (this.direction == "up") {
+            return {x: lasBox.x, y: lasBox.y - 1}
+        } else {
+            return {x: lasBox.x, y: lasBox.y + 1}
+        }
     }
 }
--- src/utils/iaSimple.js
+++ src/utils/iaSimple.js
@@ -50,6 +50,7 @@ export default class IASimple extends IA {
             if (respFromEngine == "touched") {
                 this.StateSensFoundedTank = "founded"
                 this.getDirection()
+                return this.fireKnowingDir()
             } else if (respFromEngine == "sinked") {
                 this.StateSensFoundedTank = "unknown"
                 this.currentMode = "search"
@@ -57,24 +58,29 @@ export default class IASimple extends IA {
             } else {
                 return this.fireAround(boxAround)
             }
-        }
-    }
-
-    getDirection () {
-        const lastBoxPlayed = this.getLastShot()
-        if (this.firstBoxTouchedWhenFound.x + 1 == lastBoxPlayed.x || this.firstBoxTouchedWhenFound.x -1 == lastBoxPlayed.x) {
-            this.sensFoundedTank == "horizontale"
-            if (this.firstBoxTouchedWhenFound.x + 1 == lastBoxPlayed.x) {
-                this.direction = "right"
-            } else {
-                this.direction = "left"
-            }
         } else {
-            this.sensFoundedTank == "verticale"
-            if (this.firstBoxTouchedWhenFound.y + 1 == lastBoxPlayed.y) {
-                this.direction = "down"
+            if (respFromEngine == "sinked") {
+                this.StateSensFoundedTank = "unknown"
+                this.direction = ""
+                this.currentMode = "search"
+                return this.search()
+            } else if (respFromEngine == "missed") {
+                if (this.sensFoundedTank == "horizontale") {
+                    if (this.direction == "right"){
+                        this.direction = "left"
+                    } else {
+                        this.direction = "right"
+                    }
+                } else {
+                    if (this.direction == "up"){
+                        this.direction = "down"
+                    } else {
+                        this.direction = "up"
+                    }
+                }
+                return this.fireKnowingDir()
             } else {
-                this.direction = "up"
+                return this.fireKnowingDir()
             }
         }
     }
