c1fc3a50a0cfa8e0fde855871b7fc6b2d71f729b
L0uxe <loulourxl@hotmail.com>
2023-05-10 17:26:26 +0200

    iahard

--- src/utils/IA/iaHard.js
+++ src/utils/IA/iaHard.js
@@ -10,7 +10,7 @@ import IA from "./ia";
     generateCheckerboard(){
         const rest = []
         let tankCaseProb=({x:4,y:4},{x:5,y:5},{x:6,y:6},{x:7,y:7},{x:8,y:8},{x:8,y:3},{x:3,y:3},{x:3,y:8},{x:5,y:10},{x:10,y:6},{x:5,y:1},{x:1,y:5},{x:2,y:10},{x:9,y:10},{x:9,y:1},{x:2,y:1},{x:1,y:2},{x:1,y:9},{x:10,y:9},{x:10,y:2},{x:7,y:2},{x:4,y:2},{x:2,y:4},{x:2,y:7},{x:4,y:9},{x:7,y:9},{x:9,y:7},{x:9,y:4})
-        for(let i = 28; i>tankCaseProb.length;i--){
+        for(let i = 28; i>=tankCaseProb.length;i--){
             if (i>0){
             this.listBoxToPlayForSearch.push(tankCaseProb[i])
         }else{
