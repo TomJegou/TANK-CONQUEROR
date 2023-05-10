8154a53b324f738a2dbde99b3015e6d4d0e7f6b1
L0uxe <loulourxl@hotmail.com>
2023-05-10 17:40:24 +0200

    iahard

--- src/utils/IA/iaHard.js
+++ src/utils/IA/iaHard.js
@@ -9,9 +9,9 @@ import IA from "./ia";
 
     generateCheckerboard(){
         const rest = []
-        let tankCaseProb=({x:4,y:4},{x:5,y:5},{x:6,y:6},{x:7,y:7},{x:8,y:8},{x:8,y:3},{x:3,y:3},{x:3,y:8},{x:5,y:10},{x:10,y:6},{x:5,y:1},{x:1,y:5},{x:2,y:10},{x:9,y:10},{x:9,y:1},{x:2,y:1},{x:1,y:2},{x:1,y:9},{x:10,y:9},{x:10,y:2},{x:7,y:2},{x:4,y:2},{x:2,y:4},{x:2,y:7},{x:4,y:9},{x:7,y:9},{x:9,y:7},{x:9,y:4})
-        for(let i = 28; i>=tankCaseProb.length;i--){
-            if (i>0){
+        let tankCaseProb=[{x:4,y:4},{x:5,y:5},{x:6,y:6},{x:7,y:7},{x:8,y:8},{x:8,y:3},{x:3,y:3},{x:3,y:8},{x:5,y:10},{x:10,y:6},{x:5,y:1},{x:1,y:5},{x:2,y:10},{x:9,y:10},{x:9,y:1},{x:2,y:1},{x:1,y:2},{x:1,y:9},{x:10,y:9},{x:10,y:2},{x:7,y:2},{x:4,y:2},{x:2,y:4},{x:2,y:7},{x:4,y:9},{x:7,y:9},{x:9,y:7},{x:9,y:4}]
+        for(let i = 0; i<tankCaseProb.length;i++){
+            if (i<27){
             this.listBoxToPlayForSearch.push(tankCaseProb[i])
         }else{
             for (let y = 1; y <= 10; y++) {
@@ -36,6 +36,7 @@ import IA from "./ia";
         } while (this.isAlreadyPlayed(b))
         result = b
         this.listBoxAlreadyPlayed.push(result)
+        console.log(result)
         return result
     }
 }
