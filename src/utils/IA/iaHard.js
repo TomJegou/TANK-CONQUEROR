4199cc57ebebe4c64e80400c96b1d70b3ec5358d
L0uxe <loulourxl@hotmail.com>
2023-05-10 17:25:05 +0200

    iaHard

--- src/utils/IA/iaHard.js
+++ src/utils/IA/iaHard.js
@@ -8,12 +8,23 @@ import IA from "./ia";
 
 
     generateCheckerboard(){
+        const rest = []
         let tankCaseProb=({x:4,y:4},{x:5,y:5},{x:6,y:6},{x:7,y:7},{x:8,y:8},{x:8,y:3},{x:3,y:3},{x:3,y:8},{x:5,y:10},{x:10,y:6},{x:5,y:1},{x:1,y:5},{x:2,y:10},{x:9,y:10},{x:9,y:1},{x:2,y:1},{x:1,y:2},{x:1,y:9},{x:10,y:9},{x:10,y:2},{x:7,y:2},{x:4,y:2},{x:2,y:4},{x:2,y:7},{x:4,y:9},{x:7,y:9},{x:9,y:7},{x:9,y:4})
         for(let i = 28; i>tankCaseProb.length;i--){
-            his.listBoxToPlayForSearch.push(tankCaseProb[i])
+            if (i>0){
+            this.listBoxToPlayForSearch.push(tankCaseProb[i])
         }else{
-            rest.push({x: x, y: y})
+            for (let y = 1; y <= 10; y++) {
+                for (let x = 1; x <= 10; x++) {
+                    if ((x + y)% 2 == 0) {
+                        rest.push({x: x, y: y})
+                    } else {
+                        rest.push({x: x, y: y})
+                    }
+                }
+            }
         }
+    }
         this.listBoxToPlayForSearch = [...rest, ...this.listBoxToPlayForSearch]
     }
 
